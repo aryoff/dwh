80cee1e08ae8e7113a7c0df8b25016d94407cedb
Aryo Fajar <aryoff@gmail.com>
2022-06-06 08:43:32 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/input', 'ApiController@ApiInput');
+Route::post('/input', 'App\Http\Controllers\ApiController@ApiInput');
