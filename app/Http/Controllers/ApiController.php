623fc0a83564a17ef26a540401c0ca0c00d7ecf8
Aryo Fajar <aryoff@gmail.com>
2022-06-15 11:59:10 +0700

    trace API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -171,7 +171,9 @@ class ApiController extends Controller
                     }
                     $partnerIdentityId = DB::select("INSERT INTO dwh_partner_identities(dwh_partner_id,identity,profile)VALUES(:pid,:identity::VARCHAR,$partnerProfiles) ON CONFLICT (dwh_partner_id,identity) DO UPDATE SET profile=dwh_partner_identities.profile||EXCLUDED.profile RETURNING id;", ['pid' => $partnerId, 'identity' => $partnerData->identity])[0]->id;
                     try { //masukkan data interaksi ke dalam tabel sesuai dengan field yg di deklarasikan
-                        if (!DB::insert("INSERT INTO dwh_interactions(dwh_source_id,dwh_customer_id,dwh_partner_identity_id,data) VALUES (:id,:cid,:pid,:data)", ['id' => $id, 'cid' => $customerId, 'pid' => $partnerIdentityId, 'data' => json_encode($interactionData)])) { //insert data interaksi
+                        if (DB::insert("INSERT INTO dwh_interactions(dwh_source_id,dwh_customer_id,dwh_partner_identity_id,data) VALUES (:id,:cid,:pid,:data)", ['id' => $id, 'cid' => $customerId, 'pid' => $partnerIdentityId, 'data' => json_encode($interactionData)])) { //insert data interaksi
+                            DB::insert("INSERT INTO dwh_customer_to_partner(dwh_customer_id,dwh_partner_identity_id) VALUES (:cid,:pid);", ['cid' => $customerId, 'pid' => $partnerIdentityId]);
+                        } else {
                             $inputData->dwh_source_id = $id;
                             $inputData->error = "User ID $customerId Failed";
                             Storage::append('ApiFailedInputInteraction.log', json_encode($inputData));
