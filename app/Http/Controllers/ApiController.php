730c93bf153205a180a0dbe10d995f7922f1c5ed
Aryo Fajar <aryoff@gmail.com>
2022-06-15 11:49:45 +0700

    trace API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -169,6 +169,7 @@ class ApiController extends Controller
                     } else {
                         $partnerProfiles = "'{}'::jsonb";
                     }
+                    Storage::append('ApiInputInteraction.log', "INSERT INTO dwh_partner_identities(dwh_partner_id,identity,profile)VALUES($partnerId,'$partnerData->identity',$partnerProfiles) ON CONFLICT (dwh_partner_id,identity) DO UPDATE SET profile=dwh_partner_identities.profile||EXCLUDED.profile RETURNING id;");
                     $partnerIdentityId = DB::select("INSERT INTO dwh_partner_identities(dwh_partner_id,identity,profile)VALUES(:pid,':identity',$partnerProfiles) ON CONFLICT (dwh_partner_id,identity) DO UPDATE SET profile=dwh_partner_identities.profile||EXCLUDED.profile RETURNING id;", ['pid' => $partnerId, 'identity' => $partnerData->identity])[0]->id;
                     try { //masukkan data interaksi ke dalam tabel sesuai dengan field yg di deklarasikan
                         if (!DB::insert("INSERT INTO dwh_interactions(dwh_source_id,dwh_customer_id,dwh_partner_identity_id,data) VALUES (:id,:cid,:pid,:data)", ['id' => $id, 'cid' => $customerId, 'pid' => $partnerIdentityId, 'data' => json_encode($interactionData)])) { //insert data interaksi
