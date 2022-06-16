d68e5f30931a40fa39eb8db033b8c3e3a61ee343
Aryo Fajar <aryoff@gmail.com>
2022-06-16 12:34:44 +0700

    trace API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -62,6 +62,7 @@ class ApiController extends Controller
     }
     function executeInputInteraction($source, $request, $id)
     {
+        Storage::append('ApiInputInteraction.log', 'executeInputInteraction');
         $parameter = json_decode($source[0]->parameter);
         $partnerId = $source[0]->dwh_partner_id;
         //fields convertion
