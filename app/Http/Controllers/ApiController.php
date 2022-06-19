c57a706bbdb1019d1977024225b2bbbb99c6e626
Aryo Fajar <aryoff@gmail.com>
2022-06-19 15:43:09 +0700

    rollback

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -19,23 +19,6 @@ class ApiController extends Controller
     public function ApiInputInteraction(Request $request)
     {
         $response = new \stdClass;
-        //HACK logging temp
-        // $header = '';
-        // if ($request->hasHeader(AUTHORIZATION)) {
-        //     $header = $request->header(AUTHORIZATION);
-        //     $header = base64_decode(substr($header, 6, strlen($header) - 6));
-        // }
-        // $log_debug = (object) $request->all();
-        // if (!is_object($log_debug)) {
-        //     $log_debug = new \stdClass;
-        // }
-        // $log_debug->ba_username = substr($header, 0, strpos($header, ':'));
-        // $log_debug->ba_password = substr($header, strpos($header, ':') + 1, strlen($header) - strpos($header, ':') + 1);
-        // $log_debug->source_ip = $request->ip();
-        // date_default_timezone_set('Asia/Jakarta');
-        // $log_debug->log_time = date('Y-m-d H:i:s');
-        // Storage::append('ApiInputInteraction.log', json_encode($log_debug));
-        //HACK logging temp
         $response->status = 'success';
         try {
             $id = Crypt::decrypt($request->dwh_source_id);
