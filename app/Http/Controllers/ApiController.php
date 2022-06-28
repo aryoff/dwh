89c96160c2c0ed329655420c497aef77ffd3b78b
Aryo Fajar <aryoff@gmail.com>
2022-06-28 11:30:26 +0700

    trace api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -205,7 +205,12 @@ class ApiController extends Controller
                 if (count($source) === 1) {
                     $parameter = json_decode($source[0]->parameter);
                     $response = $this->convertDataInputPartnerData($parameter, (object) $request->all());
-                    DB::insert("WITH partner AS(SELECT id FROM dwh_partner_identities WHERE identity='" . $response->identity_id . "') INSERT INTO dwh_partner_datas(dwh_partner_identity_id,dwh_partner_id,data_id,data) SELECT id," . $source[0]->dwh_partner_id . "," . $response->data_id . ",'" . json_encode($response->data) . "'::jsonb FROM partner ON CONFLICT (dwh_partner_id, data_id) DO UPDATE SET data=dwh_partner_identities.data||EXCLUDED.data,updated_at=CURRENT_TIMESTAMP;");
+                    if (property_exists($response, 'identity_id') && property_exists($response, 'data_id')) {
+                        DB::insert("WITH partner AS(SELECT id FROM dwh_partner_identities WHERE identity='" . $response->identity_id . "') INSERT INTO dwh_partner_datas(dwh_partner_identity_id,dwh_partner_id,data_id,data) SELECT id," . $source[0]->dwh_partner_id . "," . $response->data_id . ",'" . json_encode($response->data) . "'::jsonb FROM partner ON CONFLICT (dwh_partner_id, data_id) DO UPDATE SET data=dwh_partner_identities.data||EXCLUDED.data,updated_at=CURRENT_TIMESTAMP;");
+                    } else {
+                        Log::critical('Key data not found from ' . $request->ip() . ' ' . $request);
+                        $response->status = FAILED;
+                    }
                 } else { //Source select failed
                     Log::critical('Failed to authenticate from ' . $request->ip() . ' ' . $request);
                     $response->status = FAILED;
