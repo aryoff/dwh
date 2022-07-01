32a1a50d9267607fdce466935edefdf83c0a1c25
Aryo Fajar <aryoff@gmail.com>
2022-07-01 09:51:00 +0700

    trace API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -37,7 +37,7 @@ class ApiController extends Controller
         try {
             $id = Crypt::decrypt($request->dwh_source_id);
             $ip = $request->ip();
-            $source = DB::select("SELECT parameter->'field' AS parameter,dwh_partner_id FROM dwh_sources CROSS JOIN (SELECT :ip AS ip,:username AS username,:password AS password) params WHERE id = :id AND jsonb_exists(parameter->'allowed_ip', ip)", ['id' => $id, 'ip' => $ip]); //ambil parameter dari table source sesuai dengan id
+            $source = DB::select("SELECT parameter->'field' AS parameter,dwh_partner_id FROM dwh_sources CROSS JOIN (SELECT :ip AS ip) params WHERE id = :id AND jsonb_exists(parameter->'allowed_ip', ip)", ['id' => $id, 'ip' => $ip]); //ambil parameter dari table source sesuai dengan id
             if (count($source) === 1) {
                 $this->executeInputInteraction($source, (object) $request->all(), $id);
             } else { //Source select failed
